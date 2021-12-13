d197eeaee2fbad9ea186e23fe6e1270a821c4c88
Monika Mishra <mishra.moonika@gmail.com>
2021-12-13 17:40:40 +0545

    My 18th day of code

---
+++ Encapsulation1.go
@@ -0,0 +1,28 @@
+package model
+
+import "fmt"
+
+func Test() {
+
+	p := &Person{
+		Name: "test",
+		age:  21,
+	}
+	fmt.Println(p)
+	c := &company{}
+	fmt.Println(c)
+
+	fmt.Println(p.GetAge())
+	fmt.Println(p.getName())
+
+	fmt.Println(p.Name)
+	fmt.Println(p.age)
+
+	person2 := GetPerson()
+	fmt.Println(person2)
+	companyName := getCompanyName()
+	fmt.Println(companyName)
+
+	fmt.Println(companyLocation)
+	fmt.Println(CompanyName)
+}
