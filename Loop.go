404b109c29058511aa0a41f87c302e9135ce3cc9
Monika Mishra <mishra.moonika@gmail.com>
2021-12-13 17:43:54 +0545

    My 19th day of Code

---
+++ Loop.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+func main() {
+	buf := bytes.NewBufferString("one\ntwo\nthree\nfour\n")
+
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+
+				fmt.Print(line)
+				break
+			}
+			fmt.Println(err)
+			break
+		}
+		fmt.Print(line)
+	}
+}
